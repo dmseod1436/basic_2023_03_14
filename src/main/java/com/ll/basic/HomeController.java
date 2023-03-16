f0e6007586db385497429b3ae3a29c5e16afb8ac
dmswldms121 <dmswldms12@gmail.com>
2023-03-16 10:31:05 +0900

    Member Rq

--- src/main/java/com/ll/basic/HomeController.java
+++ src/main/java/com/ll/basic/HomeController.java
@@ -33,12 +33,7 @@ public class HomeController {
         this.memberService = memberService;
     }
 
-    // @GetMapping("/home/main") 의 의미
-    // 개발자가 스프링부트에게 말한다.
-    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
     @GetMapping("/home/main")
-    // @ResponseBody 의 의미
-    // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
     @ResponseBody
     public String showMain() {
         return "안녕하세요.";
@@ -58,17 +53,14 @@ public class HomeController {
 
     @GetMapping("/home/increase")
     @ResponseBody
-    public int showIncrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+    public int showIncrease() {
         count++;
         return count;
     }
 
     @GetMapping("/home/plus")
     @ResponseBody
-    // @RequestParam 의 의미
-    // 개발자가 스프링부트에게 말한다.
-    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
-    // @RequestParam 는 생략가능
+
     public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
         return a + b;
     }
@@ -225,10 +217,6 @@ public class HomeController {
     @GetMapping("/home/removePerson")
     @ResponseBody
     public String removePerson(int id) {
-        // person -> person.getId() == id
-        // 위 함수가 참인 엘리먼트(요소) 경우가 존재하면, 해당 요소를 삭제한다.
-        // removed 에는 삭제수행여부가 저장된다.
-        // 조건에 맞는걸 찾았고 삭제까지 되었다면 true, 아니면 false
         boolean removed = people.removeIf(person -> person.getId() == id);
 
         if (removed == false) {
@@ -269,7 +257,6 @@ public class HomeController {
     public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
         int countInCookie = 0;
 
-        // 고객이 가져온 쿠폰에서 count 쿠폰을 찾고 그 쿠폰의 값을 가져온다.
         if (req.getCookies() != null) {
             countInCookie = Arrays.stream(req.getCookies())
                     .filter(cookie -> cookie.getName().equals("count"))
@@ -281,12 +268,8 @@ public class HomeController {
 
         int newCountInCookie = countInCookie + 1;
 
-        // 고객이 가져온 count 쿠폰값에 1을 더한 쿠폰을 만들어서 고객에게 보낸다.
-        // 쉽게 말하면 브라우저(고객)에 저장되어 있는 count 쿠폰의 값을 1 증가시킨다.
-        // 이렇게 브라우저의 쿠키값을 변경하면 재방문시에 스프링부트가 다시 그 값을 받게 되어 있다.
         resp.addCookie(new Cookie("count", newCountInCookie + ""));
 
-        // 응답 본문
         return newCountInCookie;
     }
 
--- src/main/java/com/ll/basic/base/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic/base/boundedContext/member/controller/MemberController.java
@@ -1,20 +1,20 @@
 package com.ll.basic.base.boundedContext.member.controller;
+
+import com.ll.basic.base.rq.Rq;
 import com.ll.basic.base.logData.LogData;
 import com.ll.basic.base.boundedContext.member.entity.Member;
 import com.ll.basic.base.boundedContext.member.service.MemberService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import java.util.Arrays;
+
 @Controller
 public class MemberController {
     private final MemberService memberService;
 
-    // 생성자 주입
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
@@ -22,7 +22,9 @@ public class MemberController {
 
     @GetMapping("/member/login")
     @ResponseBody
-    public LogData login(String username, String password, HttpServletResponse resp) {
+    public LogData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
+        Rq rq = new Rq(req, resp);
+
         if (username == null || username.trim().length() == 0) {
             return LogData.of("F-3", "username(을)를 입력해주세요.");
         }
@@ -35,7 +37,7 @@ public class MemberController {
 
         if (rsData.isSuccess()) {
             Member member = (Member) rsData.getData();
-            resp.addCookie(new Cookie("loginedMemberId", member.getId() + ""));
+            rq.setCookie("loginedMemberId", member.getId());
         }
 
         return rsData;
@@ -44,13 +46,11 @@ public class MemberController {
     @GetMapping("/member/logout")
     @ResponseBody
     public LogData logout(HttpServletRequest req, HttpServletResponse resp) {
-        if (req.getCookies() != null) {
-            Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals("loginedMemberId"))
-                    .forEach(cookie -> {
-                        cookie.setMaxAge(0);
-                        resp.addCookie(cookie);
-                    });
+        Rq rq = new Rq(req, resp);
+        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+
+        if (cookieRemoved == false){
+            return LogData.of("S-2", "이미 로그아웃 상태입니다.");
         }
 
         return LogData.of("S-1", "로그아웃 되었습니다.");
@@ -58,17 +58,10 @@ public class MemberController {
 
     @GetMapping("/member/me")
     @ResponseBody
-    public LogData showMe(HttpServletRequest req) {
-        long loginedMemberId = 0;
+    public LogData showMe(HttpServletRequest req, HttpServletResponse resp){
+        Rq rq = new Rq(req, resp);
 
-        if (req.getCookies() != null) {
-            loginedMemberId = Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals("loginedMemberId"))
-                    .map(Cookie::getValue)
-                    .mapToLong(Long::parseLong)
-                    .findFirst()
-                    .orElse(0);
-        }
+        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
 
